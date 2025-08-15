26715f34c4257bca067ad45356173fc96bdbda09
Caio Mizael <caio.mizaelmartins@gmail.com>
2025-08-15 02:22:35 +0000

    Primeiro commit

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,6 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'imgur.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',            // gera site estático em /out
+  images: { unoptimized: true } // evita erro do next/image no export
 };
-
-export default nextConfig;
+module.exports = nextConfig;
