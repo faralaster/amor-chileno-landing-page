7f355e31eb36c3e30b35de58ad808742810d0a1b
Caio Mizael <caio.mizaelmartins@gmail.com>
2025-08-18 18:12:24 +0000

    <!-- 📦 Facebook Pixel --> <script>   !function(f,b,e,v,n,t,s)   {if(f.f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Fresa Amor Chileno',
@@ -18,10 +19,39 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+        <noscript>
+            <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1112007637569522&ev=PageView&noscript=1"
+            />
+        </noscript>
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1112007637569522');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "68a27860a434125984edb02b";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </body>
     </html>
   );
